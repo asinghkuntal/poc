b9b80b3440cae6c23a935ed426548290c3989fb7
ArunSingh <singharun01@outlook.com>
2021-05-19 18:26:43 +0530

    Initial Commit
    Change-Id: I95e73119e7a757996a8dfc743cd2fee7c51b0d38

---
+++ src/poc/POC.java
@@ -0,0 +1,19 @@
+package poc;
+
+public class POC {
+
+	public static void main(String[] args) {
+		
+		String fieldName = "PAT_NBR_ID";
+		String fieldLowerCase = fieldName.toLowerCase();
+		System.out.println(fieldName.toLowerCase());
+		System.out.println(fieldLowerCase.substring(0, 1).toUpperCase() + fieldLowerCase.substring(1));
+		
+		POC poc = new POC();
+		String methodName = "get" + "HelloWorld";
+	}
+	
+	public void getHelloWorld() {
+		System.out.println("Hello World");
+	}
+}
